6727175b5b46231be19f993a6d5fe480cc37eee0
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-01-06 14:54:56 +0530

    dom

---
+++ query.js
@@ -0,0 +1,11 @@
+let div=document.querySelector("div");
+div.style.height="200px";
+div.style.width="300px";
+
+div.style.backgroundColor="yellow";
+div.style.margin="20px";
+let heading=document.querySelector("h1");
+heading.style.padding="10px";
+heading.style.fontSize="18px";
+heading.style.fontWeight="bold";
+heading.style.color="blue";
